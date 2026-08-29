72611cd915ba0e4d3f6b1e25e4b7feec2d41bfeb
Yuan Gao <yuang296@gmail.com>
2026-08-29 11:00:17 -0700

    AdamW

--- assignment1-basics-main/cs336_basics/optimizer.py
+++ assignment1-basics-main/cs336_basics/optimizer.py
@@ -1,7 +1,7 @@
 from collections.abc import Callable, Iterable
+import math
 from typing import Optional
 import torch
-import math
 
 
 class SGD(torch.optim.Optimizer):
@@ -23,5 +23,39 @@ class SGD(torch.optim.Optimizer):
                 grad = p.grad.data  # Get the gradient of loss with respect to p.
                 p.data -= lr / math.sqrt(t + 1) * grad  # Update weight tensor in-place.
                 state["t"] = t + 1  # Increment iteration number.
+
+        return loss
+
+
+class AdamW(torch.optim.Optimizer):
+    def __init__(self, params, lr, betas, eps, weight_decay):
+        defaults = {"lr": lr, "betas": betas, "eps": eps, "weight_decay": weight_decay}
+        super().__init__(params, defaults)
+
+    def step(self, closure: Optional[Callable] = None):
+        loss = None if closure is None else closure()
+        for group in self.param_groups:
+            lr = group["lr"]
+            beta1 = group["betas"][0]
+            beta2 = group["betas"][1]
+            eps = group["eps"]
+            weight_decay = group["weight_decay"]
+            for p in group["params"]:
+                if p.grad is None:
+                    continue
+                state = self.state[p]
+                t = state.get("t", 0) + 1
+                m = state.get("m", torch.zeros_like(p))
+                v = state.get("v", torch.zeros_like(p))
+                grad = p.grad.data
+                alpha_t = lr * math.sqrt(1 - beta2**t) / (1 - beta1**t)
+                p.data -= p.data * lr * weight_decay
+                m = beta1 * m + (1 - beta1) * grad
+                v = beta2 * v + (1 - beta2) * grad**2
+                p.data -= alpha_t * m / (torch.sqrt(v) + eps)
+
+                state["t"] = t
+                state["m"] = m
+                state["v"] = v
 
         return loss
--- assignment1-basics-main/tests/adapters.py
+++ assignment1-basics-main/tests/adapters.py
@@ -19,6 +19,7 @@ from cs336_basics.transformer import (
     TransformerLM,
 )
 from cs336_basics.loss import cross_entropy
+from cs336_basics.optimizer import AdamW
 
 
 def run_linear(
@@ -556,7 +557,7 @@ def get_adamw_cls() -> Any:
     """
     Returns a torch.optim.Optimizer that implements AdamW.
     """
-    raise NotImplementedError
+    return AdamW
 
 
 def run_get_lr_cosine_schedule(
